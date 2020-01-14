be2567b38c662ea89e6be4291f329fa275129ea8
lizzzcai <lilcolinn@gmail.com>
2020-01-14 16:49:38 +0800

    update simduck, MallardDuck

--- Chapter_1_Into_to_Design_Patterns/SimUDuck.py
+++ Chapter_1_Into_to_Design_Patterns/SimUDuck.py
@@ -3,6 +3,7 @@
 
 # Duck Behaviors
 
+# The interface that all flying behavior classes implement
 class FlyBehavior:
     def fly(self):
         raise NotImplementedError
@@ -15,41 +16,70 @@ class QuackBehavior:
 # Implementing the Duck Behaviors
 
 # Fly Behaviors
+
+# Flying behavior implementation for ducks that DO fly 
 class FlyWithWings(FlyBehavior):
     def fly(self):
         # implements duck flying
-        pass
+        print("I'm flying !!")
 
+# Flying behavior implementation for ducks that DO NOT fly(like rubber ducks and decoy ducks)
 class FlyNoWay(FlyBehavior):
     def fly(self):
         # do nothing - can't fly!
-        pass
+        print("I can't fly")
 
 # Quack Behaviors
 class Quack(QuackBehavior):
     def quack(self):
         # implements duck quacking
-        pass
+        print("Quack")
 
 class Squeak(QuackBehavior):
     def quack(self):
         # rubber duckie squeak
-        pass
+        print("Squeak")
 
-class Quack(QuackBehavior):
+class MuteQuack(QuackBehavior):
     def quack(self):
         # do nothing - can't quack!
-        pass
+        print("<< Silence >>")
 
-# Integrating the Duck Behavior
+# Integrating the Duck Behavior // Page.15
 
 class Duck:
     def __init__(self):
+        
+        # Declare two reference variables for the behavior interface types.
+        # All duck subclasses inherit these.
         self.quackBehavior = None
         self.flyBehavior = None
 
+    def display(self):
+        raise NotImplementedError
+
+    # Delegate to the behavior class
     def performQuack(self):
         self.quackBehavior.quack()
     
     def performFly(self):
         self.flyBehavior.fly()
+
+    def swim(self):
+        print("All ducks float, even decoys!")
+
+
+class MallardDuck(Duck):
+    def __init__(self):
+        self.quackBehavior = Quack()
+        self.flyBehavior = FlyWithWings()
+    
+    def display(self):
+        print("I'm a real Mallard duck")
+
+
+if __name__ == "__main__":
+    mallard = MallardDuck()
+    mallard.display()
+    mallard.performQuack()
+    mallard.performFly()
